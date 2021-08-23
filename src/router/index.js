9aac284f2808346dc29b46ce84933d824884d433
yijunjason <317223108@qq.com>
2021-08-23 16:33:02 +0800

    bug修复

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import HelloWorld from '../components/HelloWorld.vue';
 import Page404 from '../components/404';
+import { createRouter, createWebHistory  } from 'vue-router';
 
 const routes = [
     { path: '/', component: HelloWorld },
     { path: '/404', component: Page404 },
 ];
 
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
+const router = createRouter({
+    history: createWebHistory(),
     routes,
 });
 
