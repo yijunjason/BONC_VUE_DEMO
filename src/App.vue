34b0aa2a0434d1f8bbed516edbb747e77c3b093f
yijunjason <317223108@qq.com>
2021-08-24 17:47:42 +0800

    修改导航

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+        <el-header>
+            <img class="logo" alt="Vue logo" src="./assets/logo.png">
+        </el-header>
+        <el-container>
+            <el-aside width="200px" style="text-align: left;">
+                <el-menu :default-active="path" @select="menuClick">
+                    <el-menu-item v-for="(item, index) in pathArr" v-bind:key="index" :index="item.path"
+                                  :route="{path: item.path}">
+                        <i class="el-icon-menu"/>
+                        <template #title>{{item.navName}}</template>
+                    </el-menu-item>
+
+                    <!--                    <el-menu-item index="1" :route="{path: '/'}">-->
+                    <!--                        <i class="el-icon-menu"/>-->
+                    <!--                        <template #title>首页</template>-->
+                    <!--                    </el-menu-item>-->
+
+                    <!--                    <el-menu-item index="2" v-bind:route="{path: '/404'}">-->
+                    <!--                        <i class="el-icon-menu"/>-->
+                    <!--                        <template #title>导航二</template>-->
+                    <!--                    </el-menu-item>-->
+
+                    <el-menu-item index="3">
+                        <i class="el-icon-menu"/>
+                        <template #title>导航三</template>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <!--                <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+                <router-view/>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data: function () {
+            return {
+                pathArr: [
+                    {path: '/', name: 'HelloWorld', navName: '首页'},
+                    {path: '/DataPage', name: 'DataPage', navName: 'DataPage'},
+                    {path: '/IfPage', name: 'IfPage', navName: 'IfPage'},
+                    {path: '/OnPage', name: 'OnPage', navName: 'OnPage'},
+                    {path: '/FormPage', name: 'FormPage', navName: 'FormPage'},
+                    {path: '/404', navName: '未知'},
+                ],
+                path: window.location.pathname,
+            }
+        },
+        // created() {
+        //     this.path = window.location.pathname;// 此处this.$route.path读取的值始终为/，暂时未知为什么无法正确读取path
+        // },
+        methods: {
+            menuClick: function (index, indexPath, routeResult) {
+                console.log(index, indexPath, routeResult);
+                if (index === '/') {
+                    this.$router.push({
+                        name: 'HelloWorld',
+                        params: {msg: "App to HelloWorld"}
+                    });
+                } else {
+                    this.$router.push({
+                        path: index,
+                    });
+                }
+            },
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        /*margin-top: 60px;*/
+    }
+
+    .logo {
+        height: 60px;
+        /* text-align: left; */
+        float: left;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import HelloWorld from '../components/HelloWorld.vue';
-import Page404 from '../components/404';
-import { createRouter, createWebHistory  } from 'vue-router';
+// import Page404 from '../components/404';
+import {createRouter, createWebHistory} from 'vue-router';
 
 const routes = [
-    { path: '/', component: HelloWorld },
-    { path: '/404', component: Page404 },
+    {path: '/', name:'HelloWorld', component: HelloWorld, props: true},
+    {path: '/DataPage', name:'DataPage', component: () => import('../components/DataPage'), props: true},
+    {path: '/IfPage', name:'IfPage', component: () => import('../components/IfPage'), props: true},
+    {path: '/OnPage', name:'OnPage', component: () => import('../components/OnPage'), props: true},
+    {path: '/FormPage', name:'FormPage', component: () => import('../components/FormPage'), props: true},
+    {path: '/:catchAll(.*)', name: '404', component: () => {return import("../components/404")}, props: true},
+    // {path: '/:catchAll(.*)', redirect: '/404'},
 ];
 
 const router = createRouter({
